35a54989b7a777e78685e010866f93fb3c6d2aea
pavan kumar <pavan1122pkj@gmail.com>
2024-10-28 10:47:22 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'SignLanguages',
+    version = '0.0.0',
+    author = 'Pavan',
+    author_email = 'pavan1122pkj@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
